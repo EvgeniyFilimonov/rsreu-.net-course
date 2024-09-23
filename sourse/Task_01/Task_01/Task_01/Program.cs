cf6113ed774fd730c0e2be97ef2edc29e006f880
Евгений Филимонов <evgeniyfilimonov101@gmail.com>
2024-09-23 20:38:32 +0300

    Lab1-2

---
+++ sourse/Task_01/Task_01/Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение n:");
+        int n = int.Parse(Console.ReadLine());
+
+        double an = Math.Pow(-1, n) / ((n + 1) * (n + 2) * (n + 3));
+
+        Console.WriteLine($"Значение an при n = {n} равно {an}");
+    }
+}
